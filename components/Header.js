517191ad26861e9eae3913628a3e56f5ca212140
eko <ekoptk@gmail.com>
2022-06-24 21:27:48 +0700

    add menu and profile image at the right side of the header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
-import { SearchIcon } from '@heroicons/react/outline'
+import { SearchIcon, PlusCircleIcon } from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
 
 export default function Header() {
   return (
-    <div className='flex items-center justify-between max-w-6xl'>
+    <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
 
         {/* Left */}
         <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
@@ -29,12 +30,18 @@ export default function Header() {
             <div className='absolute top-2 left-2'>
                 <SearchIcon className='h-5 text-gray-500'/>
             </div>
-            <input className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-sm' type='text' placeholder='Search'/>
+            <input className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md' type='text' placeholder='Search'/>
         </div>
 
         {/* Right */}
-        <div>
-            Right
+        <div className='flex space-x-4 items-center'>
+            <HomeIcon 
+                className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 easy-out'
+            />
+            <PlusCircleIcon 
+                className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 easy-out'
+            />
+            <img className='h-10 rounded-full cursor-pointer' src='/assets/aaa.jpg' alt='user-image' />
         </div>
     </div>
   )
