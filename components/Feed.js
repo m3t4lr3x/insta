2b9d2e93a1339e2f671fb97a98bfbd8f04e1a099
eko <ekoptk@gmail.com>
2022-06-25 19:28:28 +0700

    set grid

--- components/Feed.js
+++ components/Feed.js
@@ -4,16 +4,18 @@ import Stories from './Stories'
 
 export default function Feed() {
   return (
-    <main className='max-w-6xl mx-4 xl:mx-auto'>
-        <section>
+    <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
+        <section className='md:col-span-2 '>
             {/* Stories */}
             <Stories />
             {/* POST */}
             <Posts />
         </section>
-        <section>
+        <section className='hidden md:col-span-1'>
             {/* Mini Profile */}
+            
             {/* Suggestion */}
+
         </section>
     </main>
   )
