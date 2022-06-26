be76a1631923654a6d23901085dce0190e17e5b8
eko <ekoptk@gmail.com>
2022-06-26 09:16:37 +0700

    add key index - suggestions.js

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import minifaker from 'minifaker'
 import 'minifaker/locales/en'
+// import Image from 'next/image'
 
 export default function Suggestions() {
     const [suggestions, setSuggestions] = useState([])
@@ -22,15 +23,23 @@ export default function Suggestions() {
             <h3 className='font-bold text-gray-400'>Suggestion for you</h3>
             <button className='text-gray-600 font-semibold'>See all</button>
         </div>
-        {suggestions.map(suggestion => (
-            <div className='flex items-center justify-between mt-3'>
+        {suggestions.map((suggestion, index) => (
+            <div key={index} className='flex items-center justify-between mt-3'>
                 <img
-                    className='h-10 rounded-full border p-[2px]' 
-                    src={`https://i.pravatar.cc/150?img=${Math.ceil(Math.random() * 70 )}`}
+                    className='h-10 rounded-full border p-[2px]'
+                    src={`https://i.pravatar.cc/150?img=${Math.ceil(Math.random() * 70)}`}
                     alt='profilepicture' />
+                {/* <Image
+                        className='h-10 rounded-full border p-[2px]'
+                        src={`https://i.pravatar.cc/150?img=${Math.ceil(Math.random() * 70 )}`}
+                        alt='profilepicture'
+                        width={40}
+                        height={40}
+                    /> */}
+
                 <div className='flex-1 ml-4'>
                     <h2 className='font-semibold text-sm'>{suggestion.username}</h2>
-                    <h3 className='text-gray-400 text-sm truncate w-[230px]'>{suggestion.jobTitle}</h3>
+                    <h3 className='text-gray-400 text-xs truncate w-[230px]'>{suggestion.jobTitle}</h3>
                 </div>
                 <button className='font-semibold text-blue-400 text-sm'>Follow</button>
             </div>
